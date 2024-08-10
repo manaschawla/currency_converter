d1bd3d823cb4059d630146d7133795647717419a
manaschawla <manaschawla324@gmail.com>
2024-08-10 10:55:47 +0530

    made soome changes in code

--- currency_convert.py
+++ currency_convert.py
@@ -1,32 +1,35 @@
 """this code is for currency converter using api"""
 
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # replace it with you unique API key
-API_KEY = "enter your unique API key"
+API_KEY = "enter your unique api_key"
 # input the currency you are converting from and to
-current_currency = input("enter your currency: ")
-change_currency = input("enter the currency in which you want to change it: ")
+current_currency = input("enter your currency: ").upper()
+change_currency = input("enter the currency in which you want to change it: ").upper()
 # enter the amount you want to convert
 amount = int(input("enter the amount you want to convert: "))
 # url to get the currency exchange
-url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{current_currency}"
+url = f" https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{current_currency}"
 # send request to API for getting data
 response = requests.get(url)
+data = response.json()
+logging.debug(f"Data is: {data}")
 # checking if the response is fetched properly
 if response.status_code != 200:
-    print(f"error fetching excghange: {response.status_code}")
-data = response.json()
+    logging.info(f"error fetching excghange: {response.status_code}")
+    exit()
 # checking and handling if the given currency is not in API
-if current_currency not in data:
-    print(f"the currency: {change_currency} is not available in the API.")
+if current_currency not in data["conversion_rates"]:
+    logging.warning("the currency is not available in the API.")
 # calculating the converted_amount
-try:
+else:
     conversion_rate = data["conversion_rates"][change_currency]
     converted_amount = amount * conversion_rate
     # print the result
-    print(
+    logging.info(
         f"the amount: {amount} in this currency: {change_currency} is: {converted_amount}"
-    )
-except Exception as e:
-    print(str(e))
+        )
